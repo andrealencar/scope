c1e0c7411189ab104357d7ea012f69d19f1a21c0
André Alencar <andre@zigg.com.br>
2020-05-02 15:43:03 -0300

    Docs / Calc

--- src/main/java/org/billthefarmer/scope/calc/CalcActivity.java
+++ src/main/java/org/billthefarmer/scope/calc/CalcActivity.java
@@ -1,7 +1,5 @@
 package org.billthefarmer.scope.calc;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -10,8 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import org.billthefarmer.scope.R;
+
 
 public class CalcActivity extends AppCompatActivity {
 
@@ -33,10 +31,10 @@ public class CalcActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Integer velocidade_luz = 344;
+                Integer velocidade_luz = 340;
                 String frequencia = input_frequencia.getText().toString();
                 Double l = velocidade_luz/Double.parseDouble(frequencia);
-                String result_format = String.format("%.3f", l);
+                String result_format = String.format("%.2f", l);
                 //Log.e("res -->>: ", result_format);
 
                 result.setText(result_format);
